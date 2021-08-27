2ab554c28e5f4eda382ce0375a74333cc8958184
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2021-08-27 23:20:35 +0200

    chore: add postgres build for arm

--- postgres/build.sh
+++ postgres/build.sh
@@ -1,1 +1,9 @@
-docker build --platform linux/amd64 -t otr-db .
+
+case "$1" in
+  arm)
+  docker build --platform linux/amd64 -t otr-db -f Dockerfile.arm .
+  ;;
+  x86_64)
+  docker build --platform linux/amd64 -t otr-db .
+  ;;
+esac
