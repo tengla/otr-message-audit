69dbfe8f7d3c1a8f32b3e0e1b6a96a89958eedf6
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2021-08-28 16:16:36 +0200

    chore: set default build for build script

--- build-app.sh
+++ build-app.sh
@@ -1,16 +1,22 @@
-case "$1" in
-  build)
-    pushd frontend
-    yarn build
-    popd
-    mkdir -p backend/public
-    cp -r frontend/build/* backend/public
-    ;;
-  clean)
-    echo "cleaning up backend/public"
-    rm -rf backend/public/*
-    ;;
-  *)
-    echo "nothing to do, bye .."
-    ;;
-esac
+
+function build () {
+  case "$1" in
+    build)
+      pushd frontend
+      yarn build
+      popd
+      mkdir -p backend/public
+      cp -r frontend/build/* backend/public
+      ;;
+    clean)
+      echo "cleaning up backend/public"
+      rm -rf backend/public/*
+      ;;
+    *)
+      echo "nothing to do, bye .."
+      ;;
+  esac
+}
+
+[ -z "$1" ] && build "build" || build "$1"
+
